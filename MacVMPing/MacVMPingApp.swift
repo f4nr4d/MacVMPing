82e4d20be73877b541438a590798440191d2494c
f4n <renaud.fa@gmail.com>
2026-02-18 16:16:26 +0100

    Initial Commit

---
+++ MacVMPing/MacVMPingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacVMPingApp.swift
+//  MacVMPing
+//
+//  Created by Fabien RENAUD on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacVMPingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
